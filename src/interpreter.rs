87422ada4316fc48c645ce595f981bb42c2cbcb4
deni111BG <d1bg@users.noreply.github.com>
2025-03-18 10:56:39 +0200

    added command pwd

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::process::exit;
 
 pub(crate) fn interpret(tokens: Vec<String>) {
@@ -9,6 +10,7 @@ pub(crate) fn interpret(tokens: Vec<String>) {
 
     match tokens[0].as_str() {
         "exit" => exit(0),
+        "pwd" => println!("{}", env::current_dir().unwrap().display()),
         _ => {}
     }
 }
