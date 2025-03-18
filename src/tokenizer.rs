dc1057604809b484a5ac29badfa450ecdd559bf9
d1bg <deni111bg@gmail.com>
2025-03-18 14:12:32 +0200

    improved tokenizer - added escaped characters

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,23 +3,30 @@ pub(crate) fn tokenize(input : &str) -> Vec<String> {
     let mut curr_token = String::new();
     let mut in_quotes = false;
     let mut quoted_char = ' ';
+    let mut escape = false;
 
     for c in input.chars() {
-        match c {
-            '"' | '\'' if !in_quotes => {
-                in_quotes = true;
-                quoted_char = c;
-            }
-            c if in_quotes && c == quoted_char => {
-                in_quotes = false;
-            }
-            ' ' if !in_quotes => {
-                if !curr_token.is_empty() {
-                    tokens.push(curr_token.clone());
-                    curr_token.clear();
+        if escape {
+            curr_token.push(c);
+            escape = false;
+        } else {
+            match c {
+                '\\' => escape = true,
+                '"' | '\'' if !in_quotes => {
+                    in_quotes = true;
+                    quoted_char = c;
                 }
+                c if in_quotes && c == quoted_char => {
+                    in_quotes = false;
+                }
+                ' ' if !in_quotes => {
+                    if !curr_token.is_empty() {
+                        tokens.push(curr_token.clone());
+                        curr_token.clear();
+                    }
+                }
+                _ => { curr_token.push(c) }
             }
-            _ => { curr_token.push(c) }
         }
     }
 
