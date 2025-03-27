12d175897055fed78a3ca345406c6f6882f5cf85
Soham Nandy <soham.nandy2006@gmail.com>
2025-03-27 05:16:51 +0000

    feat: add fork/exec to spawn process

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use gethostname::gethostname;
 fn main() {
     let hostname = gethostname().to_str().unwrap().to_string();
     let user = env::var("USER") // Unix-like systems
-        .or_else(|_| env::var("USERNAME")) // Windows
         .unwrap_or_else(|_| "d1sh".to_string());
 
     env::set_var("HOST", hostname.clone());
@@ -20,13 +19,18 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
 
-        if io::stdin().read_line(&mut input).is_err() {
+        if let Ok(bytes_read) = io::stdin().read_line(&mut input) {
+            if bytes_read == 0 {
+                println!();
+                break;
+            }
+        }
+        else {
             eprintln!("Error reading input.");
             continue;
         }
 
         let input = input.trim(); // remove \n char
-
         match interpreter::interpret(tokenizer::tokenize(input)){ _ => {} }
     }
 }
