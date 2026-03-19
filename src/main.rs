a2355e502910133e6092e25978c01f540c5765cd
Denis Georgiev <d1bg@tuta.io>
2026-03-19 17:32:43 +0200

    chore: tidied up main, using a crate for username and hostname

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,20 @@ mod interpreter;
 mod commands;
 
 use std::{env, io};
+use std::error::Error;
 use std::io::Write;
-use gethostname::gethostname;
+use whoami::{hostname, username};
 use crate::interpreter::Interpreter;
 use crate::tokenizer::tokenize;
-
-fn main() {
-    let hostname = gethostname().to_str().unwrap().to_string();
-    let user = env::var("USER") // Unix-like systems
-        .unwrap_or_else(|_| "d1sh".to_string());
-
-    env::set_var("HOST", hostname.clone());
+fn main() -> Result<(), Box<dyn Error>> {
+    let hostname = hostname().unwrap_or_else(|_| "hostname".to_string());
+    let user = username().unwrap_or_else(|_| "username".to_string());
 
     loop {
         print!("[{user}@{hostname}] {} d1sh> ",
-                env::current_dir().unwrap().display());
+                env::current_dir()?.display());
 
-        io::stdout().flush().unwrap();
+        io::stdout().flush()?;
         let mut input = String::new();
 
         if let Ok(bytes_read) = io::stdin().read_line(&mut input) {
@@ -34,7 +31,6 @@ fn main() {
         }
 
         let input = input.trim(); // remove \n char
-
         let interpreter = Interpreter::new();
         let tokenized = tokenize(input);
         
@@ -48,4 +44,5 @@ fn main() {
         }
 
     }
+    Ok(())
 }
